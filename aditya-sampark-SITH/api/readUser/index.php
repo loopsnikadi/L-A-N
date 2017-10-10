d28ec51f8cf4438eb16c3088d5852a62193c44ef
aditya-jejurkar <ajejurkar33@gmail.com>
2017-10-10 18:13:01 +0530

    New file over here

--- aditya-sampark-SITH/api/readUser/index.php
+++ aditya-sampark-SITH/api/readUser/index.php
@@ -1,34 +1,36 @@
 <?php
 include ('../DB/dbConnection.php');
-$result=mysqli_query($con,"Select * from samparkdata LIMIT 5");
+
+    $sql ="Select * from samparkdata LIMIT 5";
+    $result=mysqli_query($con,$sql);
+    $results = array();
+
 
 if(mysqli_num_rows($result)>0){ 
-    while($row = mysqli_fetch_array($result)){ 
-	
-		
-            echo '<table border="1">
-            
-            <tr>
-                    <td><button id="editData">Edit</button></td>
-					<td>'.$row['id'].'</td>
-					<td>'.$row['refname'].'</td>
-					<td>'.$row['fullname'].'</td>
-                    <td>'.$row['nickname'].'</td>
-                    <td>'.$row['gender'].'</td>
-                    <td>'.$row['dob'].'</td>
-                    <td>'.$row['address'].'</td>
-                    <td>'.$row['mobile'].'</td>
-                    <td>'.$row['home'].'</td>
-                    <td>'.$row['office'].'</td>
-                    <td>'.$row['email'].'</td>
-                    <td>'.$row['qualification'].'</td>
-                    <td>'.$row['majorsub'].'</td>
-                    <td>'.$row['edustatus'].'</td>
-                    <td>'.$row['attendence'].'</td>
-                    <td>'.$row['followupname'].'</td>
-                </tr>
-        </table>';
+    
+        while($row = mysqli_fetch_assoc($result))
+        {
+           $results[] = array(
+             
+                   "id" => $row['id'],
+                   "refname" =>	$row['refname'],
+                   "fullname" => $row['fullname'],
+                   "nickname" =>  $row['nickname'],
+                   "gender" => $row['gender'],
+                   "dob" => $row['dob'],
+                   "address" =>  $row['address'],
+                   "mobile" =>  $row['mobile'],
+                   "home" =>  $row['home'],
+                   "office" =>  $row['office'],
+                   "email" =>  $row['email'],
+                    "qualification" =>    $row['qualification'],
+                    "majorsub" =>    $row['majorsub'], 
+                    "edustatus" =>   $row['edustatus'], 
+                    "attendence" =>   $row['attendence'], 
+                    "followupname" => $row['followupname'] 
+               
+           );
         }
-        mysqli_free_result($result);
-        } 
+        }
+        echo json_encode($results) ;
 ?>
